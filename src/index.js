aa18bff37aac3a33cfbf046810c84724887fe7ee
Megas-MDN <mdn.melqui@gmail.com>
2022-12-16 00:13:52 -0300

    Set url to client deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://chat-react-socketio-delta.vercel.app/',
   },
 });
 
